0921102aef2dbdd14741e052dca69818c7829a31
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-07-06 07:56:42 -0400

    Moved functions to routes file and its not working properly. Got yield working properly. About to get working on currentlyEmployed page to just make sure it works.

--- main.js
+++ main.js
@@ -11,65 +11,10 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './public/views');
 app.set('view engine', 'mustache');
 
-app.get('/users', function (req, res){
-  res.render('main', {users: proDocs});
-});
 
-app.get('unemployed', function(req, res){
-  res.render('unemployed');
-});
-
-var proDocs;
-
-var findDocuments = function(users, callback) {
-  // Get the documents collection
-  var collection = users.collection('userdata');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-      proDocs = docs;
-    console.log(docs)
-    callback(docs);
-  });
-}
-
-var findUnemployed = function(users, callback) {
-  // Get the documents collection
-  let collection = users.collection('userdata');
-  // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-      proDocs = docs;
-    console.log(docs)
-    callback(docs);
-  });
-}
+app.set('layout', 'main');
 
 
-
-
-
-
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/users';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-findDocuments(db, function() {
-  db.close();
-});
-findUnemployed(db, function(){
-  db.close();
-});
-});
 
 
 
--- routes.js
+++ routes.js
@@ -5,10 +5,68 @@ const router = express.Router();
 
 
 
+router.get('/users', function (req, res){
+  res.render('main', {users: proDocs});
+});
+
+
+router.get('/lookingForWork', function(req, res){
+  res.render('unemployed');
+});
+
+
+
+var proDocs;
+
+var findDocuments = function(users, callback) {
+  // Get the documents collection
+  var collection = users.collection('userdata');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+      proDocs = docs;
+    console.log(docs)
+    callback(docs);
+  });
+}
+
+var lookingForWork = function(users, callback) {
+  // Get the documents collection
+  let collection = users.collection('userdata');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+      proDocs = docs;
+    console.log(docs)
+    callback(docs);
+  });
+}
 
 
 
 
+
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/users';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+findDocuments(db, function() {
+  db.close();
+});
+lookingForWork(db, function(){
+  db.close();
+});
+});
 
 
 
